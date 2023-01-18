4001f21c7a0e5d1d7b5807e45177f9b76e576365
Pingbo Yang <86988793+PingboYang@users.noreply.github.com>
2023-01-18 15:43:28 -0500

    Add files via upload

---
+++ src/BoxComponent.js
@@ -0,0 +1,83 @@
+import React from "react"
+
+function Box1(){
+    return (
+        <div>
+            <ul className="box1">
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+function Box2(){
+    return (
+        <div>
+            <ul className="box2">
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+function Box3(){
+    return (
+        <div>
+            <ul className="box3">
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+function Box4(){
+    return (
+        <div>
+            <ul className="box4">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+function Box5(){
+    return (
+        <div>
+            <ul className="box5">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+function Box6(){
+    return (
+        <div>
+            <ul className="box6">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+export const boxArray=[<Box1/>,<Box2/>,<Box3/>,<Box4/>,<Box5/>,<Box6/>]
+
+
+    
+
+
